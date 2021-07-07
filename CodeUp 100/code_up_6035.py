8b187d97f47eb52a3f5756e8a1fdaa6bff715f34
seok-jong <sjhyeon1222@gmail.com>
2021-07-08 08:57:12 +0900

    CodeUp 기초 100제

---
+++ CodeUp 100/code_up_6035.py
@@ -0,0 +1,14 @@
+#
+# 실수 2개(f1, f2)를 입력받아 곱을 출력하는 프로그램을 작성해보자.
+#
+# 예시
+# ...
+# m = float(f1) * float(f2)
+# print(m)
+
+
+#code
+
+a, b = list(map(float,input().split()))
+
+print(a*b)
---
+++ CodeUp 100/code_up_6052.py
@@ -0,0 +1,12 @@
+
+# 정수가 입력되었을 때, True/False 로 평가해주는 프로그램을 작성해보자.
+#
+# 예시
+# n = int(input())
+# print(bool(n))
+
+
+#code
+
+a=int(input())
+print(bool(a))
