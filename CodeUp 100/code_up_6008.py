0dabe0e48368e707b1d2c8526855088c719cd037
seok-jong <sjhyeon1222@gmail.com>
2021-07-14 22:27:49 +0900

    codeup

---
+++ CodeUp 100/code_up_6008.py
@@ -0,0 +1,8 @@
+# 이번에는 다음과 같은 python프로그램의 소스코드를 출력해보자.
+#
+# print("Hello\nWorld")
+
+
+# source Code
+
+print("print(\"Hello\\nWorld\")")
---
+++ CodeUp 100/code_up_6040.py
@@ -0,0 +1,12 @@
+#
+# 정수 2개(a, b) 를 입력받아 a를 b로 나눈 몫을 출력해보자.
+#
+# 예시
+# ...
+# print(a//b)
+
+#code
+
+a,b=list(map(int,input().split()))
+
+print(a//b)
